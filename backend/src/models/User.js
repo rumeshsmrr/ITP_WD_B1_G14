348933f255b56e27f57deaaae39652fb7cb161cc
poornaka <99286090+IT21381218@users.noreply.github.com>
2023-04-10 11:55:08 +0530

    add user properties to the user model file

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,1 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+    },
+    userName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    salary: {
+      type: Number,
+    },
+    role: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+    age: {
+      type: Number,
+    },
+    sex: {
+      type: String,
+    },
+    systemAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    savedDate: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("user", UserSchema);
+module.exports = User;
