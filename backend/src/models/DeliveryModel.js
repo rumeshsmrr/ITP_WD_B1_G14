bf1e273ce03d1076cf460318c3e93c77a7fc32a2
IT21249952 <99280237+IT21249952@users.noreply.github.com>
2023-04-15 20:56:13 +0530

    create deliver model

---
+++ backend/src/models/DeliveryModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const deliverySchema = new mongoose.Schema({
+    
+     full_name:{
+      type:String,
+      required:true
+     },
+     delivery_address:{
+      type:String,
+      required:false
+     },
+     company_name:{
+        type:String,
+        required:false
+     },
+     land_mark:{
+        type:String,
+        required:false
+     },
+     mobile:{
+        type:String,
+        required:false
+     },
+     order_no:{
+        type:String,
+        required:false
+     },
+     date:{
+      type:String,
+      required:false
+     },
+     status:{
+      type:String,
+      required:false
+     },
+
+    });
+    
+
+module.exports = mongoose.model('Delivery',deliverySchema);
