ad600aa2595a3041fc5d887105a6cd86ef8bc73f
poornaka <99286090+IT21381218@users.noreply.github.com>
2023-04-22 11:18:28 +0530

    update user route

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const userCtrl = require("../controller/userCtrl");
+
+router.post("/register", userCtrl.addUser);
+
+router.put("/updateProfile/:id", userCtrl.updateUser);
+
+
+module.exports = router;
 
