fc58df7b85da18bf9b0d9bdf47033cbedfaacbc1
poornaka <99286090+IT21381218@users.noreply.github.com>
2023-04-22 11:42:21 +0530

    add login route

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -3,11 +3,10 @@ const router = express.Router();
 const userCtrl = require("../controller/userCtrl");
 
 router.post("/register", userCtrl.addUser);
-
+router.post("/login", userCtrl.login);
 
 router.get("/info/:id", userCtrl.getUserInfo);
 router.get("/allusers", userCtrl.allUsers);
-
 router.delete("/:id", userCtrl.deleteUser);
 router.put("/updateProfile/:id", userCtrl.updateUser);
 router.put("/updateUser/:id", userCtrl.updateUserAdmin);
