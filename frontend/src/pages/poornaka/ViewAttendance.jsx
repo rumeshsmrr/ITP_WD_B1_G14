5a26f60cc6c726b9cf8cccd4481795aff1c2e258
poornaka <99286090+IT21381218@users.noreply.github.com>
2023-04-26 10:52:13 +0530

    Update ViewAttendance.jsx

--- frontend/src/pages/poornaka/ViewAttendance.jsx
+++ frontend/src/pages/poornaka/ViewAttendance.jsx
@@ -33,7 +33,7 @@ export default function ViewAttendance() {
   };
   return (
     <div style={{backgroundImage:"url(https://res.cloudinary.com/dwcxwpn7q/image/upload/v1682448274/Untitled-2_kws0wf.png)",
-    backgroundSize:"Cover", backgroundRepeat: "no-repeat", position:"absolute", width: "1400px", height:"800px"}}>
+    backgroundSize:"Cover", backgroundRepeat: "no-repeat", position:"absolute", width: "100%", height:"100%"}}>
       <Navbar/>
       {loading && (
         <div className="container">
