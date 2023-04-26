b164425c44d72015fe53c937993f5927e204cd64
poornaka <99286090+IT21381218@users.noreply.github.com>
2023-04-26 10:47:21 +0530

    Update EditUser.jsx

--- frontend/src/pages/poornaka/EditUser.jsx
+++ frontend/src/pages/poornaka/EditUser.jsx
@@ -35,7 +35,7 @@ export default function EditUser() {
 
   return (
     <div style={{backgroundImage:"url(https://res.cloudinary.com/dwcxwpn7q/image/upload/v1682448274/Untitled-2_kws0wf.png)",
-    backgroundSize:"Cover", backgroundRepeat: "no-repeat", position:"absolute", width: "1400px", height:"800px"}}>
+    backgroundSize:"Cover", backgroundRepeat: "no-repeat", position:"absolute", width: "100%", height:"100%"}}>
       <Navbar/>
       <div className="container">
         <div className="addUser-card">
